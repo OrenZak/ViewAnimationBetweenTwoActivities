aee61ca3121f93745efaa60e2e70e34083dc6faa
orenGit <ore.n@hotmail.com>
2015-08-21 02:32:45 +0300

    compatible for 4.1 anim

--- app/src/main/java/com/chosenanimation/Activity2.java
+++ app/src/main/java/com/chosenanimation/Activity2.java
@@ -1,16 +1,45 @@
 package com.chosenanimation;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-@TargetApi(Build.VERSION_CODES.LOLLIPOP)
-public class Activity2 extends Activity {
+public class Activity2 extends ActionBarActivity {
+
+    private ImageView image;
+    private Animation fadein;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity2);
+        image = (ImageView) findViewById(R.id.pic2);
+        fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        image.startAnimation(fadein);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        this.overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public void onBackPressed() {
+        runFadeOutAnim(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        });
+    }
+
+    public void runFadeOutAnim(final Runnable endAction){
+        image.animate()
+             .setDuration(500)
+             .scaleX(0).scaleY(0).alpha(0)
+             .withEndAction(endAction);
     }
 }
--- app/src/main/java/com/chosenanimation/MainActivity.java
+++ app/src/main/java/com/chosenanimation/MainActivity.java
@@ -3,37 +3,42 @@ package com.chosenanimation;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.util.Pair;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     private ImageView image;
-    private ImageView image2;
-    private View squerred;
-
+    private Activity mThis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.pic1);
+        mThis = this;
+    }
 
-         image = (ImageView) findViewById(R.id.pic1);
-         image2 = (ImageView) findViewById(R.id.pic2);
-         squerred = findViewById(R.id.square_red);
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        image.animate().setDuration(500).scaleX(1).scaleY(1).alpha(1);
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Activity2.class);
-                 Pair<View, String> p1 = Pair.create((View)squerred, "trans");
-                 Pair<View, String> p2 = Pair.create((View) image, "trans1");
-                 Pair<View, String> p3 = Pair.create((View)image2, "trans2");
-                 ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, p1, p2, p3);
+                image.animate().setDuration(500).scaleX(0).scaleY(0).alpha(0).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(MainActivity.this, Activity2.class);
+                        startActivity(i);
+                        mThis.overridePendingTransition(0, 0);
+                    }
+                });
 
-                startActivity(i, options.toBundle());
+
             }
         });
     }
+
 }
